ece83521baccf4cce8e1792b4cf3040d9cbb3f38
Neto test <ehimovsergei@mail.ru>
2023-12-23 14:42:51 +0300

    Initial commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final ExecutorService pool = Executors.newFixedThreadPool(64);
+    private final Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
+
+    public void listen(int port) {
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    var socket = serverSocket.accept();
+                    var connectHandler = new ConnectHandler(socket, handlers);
+                    pool.execute(connectHandler::handle);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHandler(String method, String path, Handler handler) {
+        Map<String, Handler> map = new ConcurrentHashMap<>();
+        if (handlers.containsKey(method)) {
+            map = handlers.get(method);
+        }
+        map.put(path, handler);
+        handlers.put(method, map);
+    }
+}
